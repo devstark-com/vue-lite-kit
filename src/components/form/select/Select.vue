d7c884b0747ed439fa67b0b8c247a13a2a0c9a02
Saymon <saymon.biz@gmail.com>
2018-03-16 12:28:21 +0300

    Add keyboard navigation

--- src/components/form/select/Select.vue
+++ src/components/form/select/Select.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    tabindex="0"
     class="vlk-select"
     :class="{
       'selected': selectedOption !== null,
@@ -8,6 +9,13 @@
     }"
     v-on-clickaway="closeDropdown"
     @click="toggleDropdown"
+    @keydown.up="onArrowUp"
+    @keydown.down="onArrowDown"
+    @keydown.enter="onEnter"
+    @keydown.tab="onTab"
+    @keydown.esc="onEsc"
+    @focus="onFocus"
+    @blur="onBlur"
   >
     <label v-if="label">{{ label }}</label>
     <div class="vlk-form-control">
@@ -20,14 +28,17 @@
     <ul class="vlk-options" v-if="opened">
       <slot
         name="option"
-        v-for="option in options"
+        v-for="option in optionsList"
         :option="option"
-        :options="options"
+        :options="optionsList"
       >
         <li
-          @click.stop="optionClicked(option.value)"
+          @click.stop="onOptionClicked(option.index)"
           :key="option.value"
-          :class="{'current-option': (selectedOption && option.value === selectedOption.value) }
+          :class="{
+            'selected': (selectedOption && option.value === selectedOption.value),
+            'current': (currentOption && option.value === currentOption.value)
+          }
         ">
           {{ option.label }}
         </li>
@@ -53,49 +64,160 @@ export default {
     options: {
       type: Array,
       default: () => []
+    },
+    openOnFocus: {
+      type: Boolean,
+      default: false
+    },
+    selectOnClose: {
+      type: Boolean,
+      default: false
     }
   },
   data () {
     return {
       opened: false,
-      selectedOption: null
+      currentOptionIndex: null,
+      selectedOptionIndex: null
+    }
+  },
+  computed: {
+    optionsList () {
+      return this.options.map((item, index) => {
+        return {
+          index,
+          ...item
+        }
+      })
+    },
+
+    firstOption () {
+      return this.optionsList[0]
+    },
+
+    lastOption () {
+      const lastIndex = this.optionsList.length - 1
+      return this.optionsList[lastIndex]
+    },
+
+    selectedOption () {
+      return this.findOptionByField('index', this.selectedOptionIndex)
+    },
+
+    currentOption () {
+      return this.findOptionByField('index', this.currentOptionIndex)
     }
   },
   watch: {
     value (val, old) {
-      this.selectOption(val)
+      this.setSelectedByValue(val)
+    },
+
+    optionsList () {
+      // @todo reset appropriate props in data when options list changed in the parent component
     }
   },
   methods: {
-    toggleDropdown () {
-      this.opened = !this.opened
+    onArrowUp () {
+      if (!this.opened) return this.openDropdown()
+      this.prev()
     },
 
-    closeDropdown () {
-      this.opened = false
+    onArrowDown () {
+      if (!this.opened) return this.openDropdown()
+      this.next()
     },
 
-    optionClicked (value) {
+    onEnter () {
+      if (!this.opened) return this.openDropdown()
+      this.selectOption(this.currentOptionIndex)
       this.closeDropdown()
-      this.selectOption(value)
     },
 
-    getOptionByValue (value) {
-      return this.options.filter((item) => {
-        return item.value === value
-      })[0]
+    onTab () {
+      if (this.opened) return this.closeDropdown()
     },
 
-    selectOption (value) {
-      const option = this.getOptionByValue(value)
-      // if (!option) throw new Error('Option with value "' + value + '" wasn\'t found')
-      if (!option) return
-      this.selectedOption = option
+    onEsc () {
+      if (this.opened) return this.closeDropdown()
+    },
+
+    onFocus () {
+      if (this.openOnFocus && !this.opened) this.openDropdown()
+      this.$emit('focus')
+    },
+
+    onBlur () {
+      this.$emit('blur')
+    },
+
+    onOptionClicked (index) {
+      this.selectOption(index)
+      this.closeDropdown(true)
+    },
+
+    prev () {
+      const prevOptionIndex = this.currentOptionIndex - 1
+      const prevOption = this.findOptionByField('index', prevOptionIndex)
+      const newCurrentOption = prevOption || this.lastOption
+      this.setCurrentOption(newCurrentOption.index)
+    },
+
+    next () {
+      const nextOptionIndex = this.currentOptionIndex + 1
+      const nextOption = this.findOptionByField('index', nextOptionIndex)
+      const newCurrentOption = nextOption || this.firstOption
+      this.setCurrentOption(newCurrentOption.index)
+    },
+
+    toggleDropdown () {
+      this.opened ? this.closeDropdown() : this.openDropdown()
+    },
+
+    openDropdown () {
+      const currentOptionIndex = this.selectedOptionIndex || 0
+      this.setCurrentOption(currentOptionIndex)
+      this.opened = true
+      this.$emit('open')
+    },
+
+    closeDropdown (preventSelectOnClose = false) {
+      if (this.selectOnClose && !preventSelectOnClose && this.currentOptionIndex !== this.selectedOptionIndex) {
+        this.selectOption(this.currentOptionIndex)
+      }
+      this.opened = false
+      this.$emit('close')
+    },
+
+    setCurrentOption (index) {
+      this.currentOptionIndex = index
+      this.$emit('current', this.currentOption.value)
+    },
+
+    setSelectedByValue (value) {
+      const selectedOption = this.findOptionByField('value', value)
+      if (!selectedOption) return // @todo fire an error
+      this.selectOption(selectedOption.index)
+    },
+
+    selectOption (index) {
+      const option = this.findOptionByField('index', index)
+      if (!option) {
+        console.warn('Option with index "' + index + '" wasn\'t found')
+        return
+      }
+      this.selectedOptionIndex = option.index
       this.$emit('input', option.value)
+    },
+
+    findOptionByField (fieldName, value) {
+      return this.optionsList.filter((item) => {
+        return item[fieldName] === value
+      })[0]
     }
   },
   created () {
-    this.selectOption(this.value)
+    this.setSelectedByValue(this.value)
   }
 }
 </script>
